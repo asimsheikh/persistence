c36c116a7c97abdce66b19b263e9501d0383a884
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-20 10:57:36 +1100

    Loading db.json file if available

--- src/persist.py
+++ src/persist.py
@@ -1,21 +1,36 @@
 import json
+from pathlib import Path
 from typing import Any 
 
 class Persist:
     def __init__(self, fname: str ='db.json'):
-        self.data: dict[str,list[dict[Any, Any]]]= dict()
         self.fname = fname
+        fname_path = Path(fname)
+
+        if fname_path.exists():
+            with open(fname) as f:
+                self.data = json.load(f)
+        else:
+            self.data: dict[str,list[dict[Any, Any]]]= dict()
             
     def _save(self):
         with open(self.fname, 'w') as fn:
             json.dump(self.data, fn)
+
     def add(self, key: str, data: dict):
         if self.data.get(key):
             self.data[key].append(data)
         else:
             self.data[key] = [data]
         self._save()
+
     def get(self, key: str):
         return self.data[key]
+
     def get_all(self):
         return self.data
+
+    def clear(self):
+        self.data = {}
+        self._save()
+
